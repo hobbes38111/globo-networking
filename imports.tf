2ddf89ac05eff79645efef8b1abc3551d42cd622
Bojan Zivkovic <zbojanb@gmail.com>
2024-02-12 13:07:08 +0100

    initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-02fc6bae26d89dc1f" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-052cadfbc3ce8688d" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-0e2c994cb4f958f76" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0439736f97c65f9c3" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0f32a1043497617ac_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0f32a1043497617ac" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-052cadfbc3ce8688d/rtb-0f32a1043497617ac" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-0e2c994cb4f958f76/rtb-0f32a1043497617ac" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-0a6aedb0bcf75a34d" #NoIngressSecurityGroup
+# }
